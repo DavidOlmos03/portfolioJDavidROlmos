959cfec8a0b15d5649aadf57d3c489fc3f694922
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-01-29 18:22:39 -0500

    style and js for project_day6

--- src/assets/courseJS/project_day6/boletinEstudiante.js
+++ src/assets/courseJS/project_day6/boletinEstudiante.js
@@ -1,50 +1,80 @@
-let array = [4,4.8,5,4,3];
-/*
- *Crete the elements <li> for to have all notes
- */
-function listNotes(){
-    let list = document.getElementById("ulNotes");
 
-    for(let note of array){
-        let item = document.createElement("li");
-        item.innerText = note;
-        list.appendChild(item);
-    }
+let sum;
+let array = []
+let numbersQuantity = document.getElementById("numbersQuantity");
+
+function obtainNotes(){
+  for(let i=0;i<this.numbersQuantity.value;i++){
+    let input = document.getElementById("number"+(i+1));
+    array[i] = input.value;
+  }
 }
-/**
- * Calculate the average between all notes 
+
+/*
+ * Generate the numbers
+ */
+function generateInputNumbers(){
+  numbers = this.numbersQuantity.value
+  let form = document.getElementById("formulario");
+
+  // Eliminar todos los inputs existentes del formulario
+  form.innerHTML = '';
+
+  let placeholders = ["First note", "Second note", "Third note", "Fourth note", "Fifth note","Sixth note","Seventh note","Eighth","Ninth note", "Tenth note"];
+
+  for(let i=0;i<numbers;i++){
+    let item = document.createElement("input");
+    item.id = "number" + (i+1);
+    item.type = "number";
+    item.className = "form-control";
+    item.placeholder = placeholders[i];
+    form.appendChild(item);
+  }
+}
+/*
+ * Calculate the average between all notes
  */
 function average(){
+
+    this.obtainNotes();
+
+    let arrayNew = array;
     let sum = 0;
 
-    for(let item of array){
-        sum = sum+item;
+    for(let item of arrayNew){
+        sum = sum+Number(item);
     }
-    average = sum/5;
-    document.getElementById("average").textContent = average;
+    let averageValue = sum/arrayNew.length;
+    document.getElementById("average").textContent = averageValue;
 }
-/**
+/*
  * Calculate the best note
  */
 function bestNote(){
+    this.obtainNotes();
+    let arrayNew = array;
+
     let best = 0;
     let i = 0;
     while(i<5){
-        if(array[i]>best){
-            best = array[i];
+        if(Number(arrayNew[i])>best){
+            best = arrayNew[i];
         }
         i++;
     }
     document.getElementById("bestNote").textContent = best;
 }
-/**
+/*
  * Calculate if exist some "aplazo"
  */
 function aplazo(){
+  this.obtainNotes();
+  let arrayNew = array;
+
     let flag = "No exist";
     let i = 0;
     do{
-        if(array[i] < 3){
+        if(Number(arrayNew[i]) < 3){
             flag = "exist";
             break;
         }
