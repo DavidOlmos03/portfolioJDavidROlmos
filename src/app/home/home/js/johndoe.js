62fc4e828c58513dcdf04dfdf222dddf6726590e
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-03-31 23:34:27 -0500

    some traductions added

--- src/app/home/home/js/johndoe.js
+++ src/app/home/home/js/johndoe.js
@@ -13,23 +13,24 @@
 */
 
 // smooth scroll
+/*
 $(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
+  $(".navbar .nav-link").on('click', function(event) {
 
-        if (this.hash !== "") {
+      if (this.hash !== "") {
 
-            event.preventDefault();
+          event.preventDefault();
 
-            var hash = this.hash;
+          var hash = this.hash;
 
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function(){
-                window.location.hash = hash;
-            });
-        } 
-    });
-});
+          $('html, body').animate({
+              scrollTop: $(hash).offset().top
+          }, 700, function(){
+              window.location.hash = hash;
+          });
+      }
+  });
+});*/
 
 // protfolio filters
 $(window).on("load", function() {
