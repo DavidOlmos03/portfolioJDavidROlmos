a46a33247e2116a1454c67df9ae12f39f0431ef2
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-05-06 23:38:18 -0500

    contact component created

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css',"../home/css/johndoe.css","../home/vendors/themify-icons/css/themify-icons.css"]
 })
 export class ContactComponent {
 
