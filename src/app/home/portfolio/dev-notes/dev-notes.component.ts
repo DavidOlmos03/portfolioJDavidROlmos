2e4b6f6f94a8c8b4ebd5acdeac51b7e95d9b5c6c
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-07-05 09:55:35 -0500

    some changes in dev-notes content

--- src/app/home/portfolio/dev-notes/dev-notes.component.ts
+++ src/app/home/portfolio/dev-notes/dev-notes.component.ts
@@ -74,6 +74,25 @@ export class DevNotesComponent {
   cola = deque()
   for tarea in tareas:
       cola.append(tarea[1])`
+
+  dequeExample = `  from collections import deque
+  tareas.sort()
+  print("==Tareas ordenadas==")
+  for tarea in tareas:
+      print(tarea[1])`
+
+  formatExample = `  v = "otro texto"
+  n = 10
+  print( "Un texto '{1}' y un número '{0}'".format(v,n) )`
+
+  argsWord = `  def super_funcion(*args,**kwargs):
+  t = 0
+  for arg in args:
+      t += arg
+      print(f"Sumatorio indeterminado es = {t}")
+
+  for kwarg in kwargs:
+      print(f"{kwarg} = {kwargs[kwarg]}")`
   // Docker
   createNetwork = `docker network create name_network`
   dockerComposeUp = `docker compose -f .\docker\docker-compose.dev.yml up --build`
