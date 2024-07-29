26a7f5057d4e85c6ff39328beb70667827eb9750
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-07-28 19:30:17 -0500

    dark theme edited

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-
+import { ThemeService } from '../services/theme.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,8 +12,15 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./home.component.css',"home/css/johndoe.css"]
 })
 export class HomeComponent {
+  isDarkThemeActive:any;
+  themeSelected: any = '';
 
-  constructor(private translate:TranslateService){
+
+
+  constructor(
+    private translate:TranslateService,
+    private themeService: ThemeService
+  ){
     this.translate.addLangs(['en','es']);
     const lang = this.translate.getBrowserLang();
     if(lang !== 'en' && lang !== 'es'){
@@ -22,4 +29,21 @@ export class HomeComponent {
       this.translate.use(lang);
     }
   }
+
+  ngOnInit(): void {
+    // Leer el valor de themeSelected del localStorage
+    const themeSelected = localStorage.getItem('theme-selected');
+    // console.log(themeSelected)
+    if (themeSelected !== null) {
+      if (themeSelected === 'dark') {
+        this.isDarkThemeActive = 'dark';
+      } else {
+        this.isDarkThemeActive = 'light';
+      }
+    }
+    this.themeService.themeSelected$.subscribe(theme => {
+      this.themeSelected = theme;
+    });
+  }
+
 }
