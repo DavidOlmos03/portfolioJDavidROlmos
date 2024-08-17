975b65daf6012247b86fec19b399012a46ebb288
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-08-17 11:10:00 -0500

    carousel footer

--- src/app/home/portfolio/carousel/carousel.component.ts
+++ src/app/home/portfolio/carousel/carousel.component.ts
@@ -1,7 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit, HostListener } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, HostListener,inject, PLATFORM_ID, NgIterable } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import {NgFor, NgIf} from '@angular/common';
-import {TuiIcon} from '@taiga-ui/core';
+import {CommonModule, NgFor, NgIf, isPlatformBrowser} from '@angular/common';
+import {TuiIcon, TuiButton} from '@taiga-ui/core';
 import {TuiCarousel, TuiPagination} from '@taiga-ui/kit';
 import { StreamingNetflixModalComponent } from '../streaming-netflix-modal/streaming-netflix-modal.component';
 import { VuejsModalComponent } from '../vuejs-modal/vuejs-modal.component';
@@ -16,6 +16,8 @@ import { WebSiteEchezModalComponent } from '../web-site-echez-modal/web-site-ech
     TuiCarousel,
     TuiIcon,
     TuiPagination,
+    CommonModule,
+    // TuiButton,
     NgFor,
     NgIf
   ],
@@ -26,6 +28,8 @@ import { WebSiteEchezModalComponent } from '../web-site-echez-modal/web-site-ech
 export class CarouselComponent implements OnInit{
     // private index = 0;
     protected index = 0;
+    // protected duration = isPlatformBrowser(inject(PLATFORM_ID)) ? 4_000 : 0;
+    // protected rounded = 0;
     protected itemsCount = 3;
 
     constructor(
@@ -35,6 +39,7 @@ export class CarouselComponent implements OnInit{
     // Revisa el cambio en el tamaño de la ventana
     ngOnInit(): void {
       this.setItemsCount(window.innerWidth);
+      // console.log(this.items)
     }
 
     @HostListener('window:resize', ['$event'])
@@ -53,72 +58,124 @@ export class CarouselComponent implements OnInit{
       }
     }
 
+
+
     protected readonly items = [
         {
-          title: 'Laravel & Angular', Image: 'laravel-angular-course.jpg', subtitle: 'Tecnologías',
+          title: 'Laravel-Angular', Image: 'laravel-angular-course.jpg', subtitle: 'Tecnologías',
           linkGit: 'https://github.com/DavidOlmos03/streaming_tipo-netflix',
           btnModal: 'StreamingNetflixModal',
+          icons:{
+            html5:'HTML5',
+            css:'CSS',
+            bootstrap:'Bootstrap',
+            angular:'Angular',
+            laravel:'Laravel',
+            php:'PHP'
+          }
         },
 
         {
           title: 'Vue js', Image: 'vuejs-course.jpg', subtitle: 'Tecnologías',
           linkGit: 'https://github.com/DavidOlmos03/course-vuejs/tree/main',
           btnModal: 'VuejsModal',
+          icons:{
+            vuejs:'VueJS',
+            css:'CSS',
+            tailwind:'Tailwind',
+            firebase:'Firebase',
+            expressjs:'Express',
+            nodejs:'NodeJs',
+            mongodb: 'mongoDB'
+          }
         },
 
         {
           title: 'DevNotes', Image: 'devNotes.png', subtitle: 'Tecnologías',
-          linkGit: 'https://github.com/DavidOlmos03/portfolioJDavidROlmos/tree/main/src/app/home/portfolio/dev-notes'
+          linkGit: 'https://github.com/DavidOlmos03/portfolioJDavidROlmos/tree/main/src/app/home/portfolio/dev-notes',
+          btnPage: 'https://davidolmos-portfolio.netlify.app/dev-notes/angular',
+          icons:{
+            html5:'HTML5',
+            css:'CSS',
+            bootstrap:'Bootstrap',
+            angular:'Angular'
+          }
         },
 
         {
-          title: 'Docker', Image: 'docker-wall-1.jpeg', subtitle: 'Tecnologías'
+          title: 'Docker', Image: 'docker-wall-1.jpeg', subtitle: 'Tecnologías',
+          icons:{
+            docker:'DOCKER'
+          }
         },
 
         {
           title: 'Python', Image: 'python-course.jpg', subtitle: 'Tecnologías',
-          linkGit: 'https://github.com/DavidOlmos03/cursoPythonHectorC'
+          linkGit: 'https://github.com/DavidOlmos03/cursoPythonHectorC',
+          icons:{
+            python:'Python'
+          }
         },
 
         {
-          title: 'mongoDB', Image: 'courseMongodb.jpg', subtitle: 'Tecnologías'
+          title: 'mongoDB', Image: 'courseMongodb.jpg', subtitle: 'Tecnologías',
+          btnCertificate: 'assets/docs/certificados/mongoDB.pdf',
+          icons:{
+            mongodb:'mongoDB'
+          }
         },
 
         {
           title: 'javaScript', Image: 'course-1.png', subtitle: 'Tecnologías',
-          linkGit: 'https://github.com/DavidOlmos03/Udemy_CursoJS'
+          linkGit: 'https://github.com/DavidOlmos03/Udemy_CursoJS',
+          btnPage: 'https://davidolmos-portfolio.netlify.app/course-js',
+          btnCertificate: 'assets/docs/certificados/javaScriptFG.pdf',
+          icons:{
+            html5:'HTML5',
+            css:'CSS',
+            javascript:'javaScript'
+          }
         },
 
         {
-          title: 'SQL Server', Image: 'courseSQLServer.jpg', subtitle: 'Tecnologías'
+          title: 'SQL Server', Image: 'courseSQLServer.jpg', subtitle: 'Tecnologías',
+          btnCertificate: '',
         },
 
         {
-          title: 'postgreSQL', Image: 'coursePostgreSQL.png', subtitle: 'Tecnologías'
+          title: 'postgreSQL', Image: 'coursePostgreSQL.png', subtitle: 'Tecnologías',
+          btnCertificate: 'assets/docs/certificados/postgreSQL.pdf',
         },
 
         {
-          title: 'SQL', Image: 'courseSQL.jpg', subtitle: 'Tecnologías'
+          title: 'SQL', Image: 'courseSQL.jpg', subtitle: 'Tecnologías',
+          btnCertificate: 'assets/docs/certificados/SQL.pdf',
         },
 
         {
           title: 'mundoSteam', Image: 'mundoSteam.png', subtitle: 'Tecnologías',
-          linkGit: 'https://github.com/DavidOlmos03/mundoStem'
+          linkGit: 'https://github.com/DavidOlmos03/mundoStem',
+          btnPage: 'https://mundostem.netlify.app/',
+          icons:{
+            html5:'HTML5',
+            css:'CSS',
+            angular:'Angular',
+            python:'Python',
+            typescript:'typeScript'
+          }
         },
 
         {
           title: 'webSiteEchez', Image: 'webSiteEchez.png', subtitle: 'Tecnologías',
           linkGit: 'https://github.com/DavidOlmos03/webSiteEchez',
-          btnModal: 'WebSiteEchezModal'
+          btnModal: 'WebSiteEchezModal',
+          icons:{
+            html5:'HTML5',
+            css:'CSS',
+            javascript:'javaScript',
+            php:'PHP'
+          }
         },
-
-
-        // 'courseSQL.jpg',
-        // 'avatar.jpg',
-        // 'angular.svg',
-        // 'avatar.jpg',
-        // 'courseSQL.jpg',
-        // 'avatar.jpg',
     ];
 
     protected get rounded(): number {
@@ -126,11 +183,12 @@ export class CarouselComponent implements OnInit{
     }
 
     protected onIndex(index: number): void {
-        this.index = index * this.itemsCount;
+      this.index = index * this.itemsCount;
+      // this.rounded = Math.round(index);
     }
 
 
-    mostrarModal(modalName:any){
+    protected mostrarModal(modalName:any){
       // console.log(modalName)
       if(modalName=='VuejsModal'){
         const modalRef = this.modalService.open(
